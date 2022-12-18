c57a6cb81c0a9dfd74f33d04d32163bac3050ba6
filxxip <f.poltoraczyk@gmail.com>
2022-12-18 08:10:45 +0100

    Adding some docs and some changes in creaure classes (base for future builder/factory class)

--- src/game/creature/Monster.java
+++ src/game/creature/Monster.java
@@ -1,4 +1,9 @@
 package game.creature;
 
+import game.interfaces.Statistics;
+
 public class Monster extends Creature{
+    public Monster(Statistics creatureStats) {
+        super(creatureStats);
+    }
 }
