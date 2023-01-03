b70e4695327f806b4314888586955a0b30e601da
Pakos <pakostasip@gmail.com>
2023-01-03 17:13:16 +0100

    Zrobiłem klasy do każdego  rodzaju przedmiotu

---
+++ src/game/equipment/Armor.java
@@ -0,0 +1,9 @@
+package game.equipment;
+
+import game.interfaces.Statistics;
+
+public class Armor extends ManyUsageItem{
+    public Armor(Statistics statistics) {
+        super(statistics);
+    }
+}
