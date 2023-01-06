c7288e1f7fab2e41701004babe678f15d5a64c46
protectedvoid21 <nieznanyzero@interia.pl>
2023-01-06 16:11:57 +0100

    NPC constructor fix; remove struggle attributes from Statistics

--- src/game/interfaces/StruggleStatistics.java
+++ src/game/interfaces/StruggleStatistics.java
@@ -6,16 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StruggleStatistics implements IStruggleStatistics {
-
-
     protected Map<IStruggleAtributeEnum, AttributeValue> struggleAttributes = new HashMap<>();
-
 
     public StruggleStatistics() {
         initializeStruggleStatistics();
     }
 
-    protected void initializeStruggleStatistics() {};
+    protected void initializeStruggleStatistics() {}
 
     @Override
     public AttributeValue getAttribute(IStruggleAtributeEnum iStruggleAtributeEnum) {
