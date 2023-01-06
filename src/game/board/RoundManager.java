9673991ac8e68b139b0e298254ae39b399788df0
protectedvoid21 <nieznanyzero@interia.pl>
2023-01-06 13:42:41 +0100

    RoundManager now gets all active GameObjects from Board each turn

--- src/game/board/RoundManager.java
+++ src/game/board/RoundManager.java
@@ -3,10 +3,15 @@ package game.board;
 import java.util.List;
 
 public class RoundManager {
+    private final Board board;
     private List<GameObject> activeGameObjects;
     
     private int currentTurn = 1;
     private int currentIndex;
+    
+    public RoundManager(Board board) {
+        this.board = board;
+    }
     
     public GameObject getGameObjectWithTurn() {
         GameObject currentObject = activeGameObjects.get(currentIndex);
@@ -30,6 +35,7 @@ public class RoundManager {
     
     public void startNewTurn() {
         currentTurn++;
+        activeGameObjects = board.getAllGameObjects();
     }
     
     public int getCurrentTurn() {
