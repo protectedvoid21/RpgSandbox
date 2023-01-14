84a2d13bf28e031a32b448a7e0c37f397004606d
Filpus <fil-pus@wp.pl>
2023-01-14 14:45:44 +0100

    Wyłączanie Guzików

---
+++ src/gui/actionListener/warhammer/EndTurnListener.java
@@ -0,0 +1,27 @@
+package gui.actionListener.warhammer;
+
+import game.board.RoundManager;
+import game.creature.Creature;
+import gui.actionListener.turnOffButtons;
+import gui.views.Point;
+import gui.views.gamePanel.MainPanelGame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EndTurnListener implements ActionListener {
+
+    RoundManager roundManager;
+    MainPanelGame mainPanelGame;
+
+    public EndTurnListener(RoundManager roundManager, MainPanelGame mainPanelGame) {
+        this.roundManager = roundManager;
+        this.mainPanelGame = mainPanelGame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        roundManager.startNewTurn();
+    }
+}
