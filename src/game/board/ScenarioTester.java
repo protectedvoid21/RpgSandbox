ad6627ae0a4b2e0c6e11d2635dd079b7aeac98c7
protectedvoid21 <nieznanyzero@interia.pl>
2023-01-21 21:02:17 +0100

    Use EntityManager instead of FileManager

--- src/game/board/ScenarioTester.java
+++ src/game/board/ScenarioTester.java
@@ -4,6 +4,7 @@ import game.creature.Experience;
 import game.creature.Monster;
 import game.creature.PlayerCharacter;
 import game.equipment.Inventory;
+import game.filehandle.EntityManager;
 import game.filehandle.FileManager;
 import game.generals.Vector2;
 import game.interfaceWarhammer.StatisticsWarhammer;
@@ -13,8 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ScenarioTester {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         List<ScenarioData> scenarioData = new ArrayList<>();
 
         scenarioData.add(new ScenarioData());
@@ -22,24 +22,19 @@ public class ScenarioTester {
         scenarioData.add(new ScenarioData());
         scenarioData.add(new ScenarioData());
 
-        scenarioData.get(0).position=new Vector2(1,1);
-        scenarioData.get(0).creature=new Monster(new StatisticsWarhammer(),new Experience(0),new StruggleStatisticsWarhammer());
+        scenarioData.get(0).position = new Vector2(1, 1);
+        scenarioData.get(0).creature = new Monster(new StatisticsWarhammer(), new Experience(0), new StruggleStatisticsWarhammer());
 
-        scenarioData.get(1).position=new Vector2(2,2);
-        scenarioData.get(1).creature=new Monster(new StatisticsWarhammer(),new Experience(1),new StruggleStatisticsWarhammer());
+        scenarioData.get(1).position = new Vector2(2, 2);
+        scenarioData.get(1).creature = new Monster(new StatisticsWarhammer(), new Experience(1), new StruggleStatisticsWarhammer());
 
-        scenarioData.get(2).position=new Vector2(5,5);
-        scenarioData.get(2).creature=new Monster(new StatisticsWarhammer(),new Experience(2),new StruggleStatisticsWarhammer());
+        scenarioData.get(2).position = new Vector2(5, 5);
+        scenarioData.get(2).creature = new Monster(new StatisticsWarhammer(), new Experience(2), new StruggleStatisticsWarhammer());
 
-        scenarioData.get(3).position=new Vector2(0,0);
-        scenarioData.get(3).creature=new PlayerCharacter(new StatisticsWarhammer(),new Inventory(), new Experience(10), new StruggleStatisticsWarhammer());
-
-        Scenario scenario=new Scenario(10,10,scenarioData);
-
-        FileManager fileManager = new FileManager("Warhammer");
-        List<Scenario> scenarioList = new ArrayList<>();
-        scenarioList.add(scenario);
-
-        fileManager.writeToFile(scenarioList,Scenario.class);
+        scenarioData.get(3).position = new Vector2(0, 0);
+        scenarioData.get(3).creature = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(10), new StruggleStatisticsWarhammer());
+        
+        Scenario scenario = new Scenario(10, 10, scenarioData);
+        EntityManager.getInstance().addScenario(scenario);
     }
 }
