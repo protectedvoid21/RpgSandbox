979e9056b0ba8f476f8558caf00f945a75dc6d07
Pakos <pakostasip@gmail.com>
2023-01-22 23:48:39 +0100

    zaślepka Disposable Itemów XD

---
+++ src/game/equipment/examples/DeadRat.java
@@ -0,0 +1,9 @@
+package game.equipment.examples;
+
+import game.equipment.DisposableItem;
+
+public class DeadRat extends DisposableItem {
+    public DeadRat(int usageCount) {
+        super(usageCount);
+    }
+}
