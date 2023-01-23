b9fccf68425055908bc743542472ed9972949693
Pakos <pakostasip@gmail.com>
2023-01-23 01:17:22 +0100

    dodałem seedery

--- src/game/utils/seeders/MonsterSeeder.java
+++ src/game/utils/seeders/MonsterSeeder.java
@@ -15,9 +15,15 @@ public class MonsterSeeder implements Seeder {
         monster2.setName("Kaspar");
         Monster monster3 = new Monster(new StatisticsWarhammer(), new Experience(0), new StruggleStatisticsWarhammer());
         monster3.setName("Krzysztof");
+        Monster monster4 = new Monster(new StatisticsWarhammer(), new Experience(0), new StruggleStatisticsWarhammer());
+        monster4.setName("Andrzej");
+        Monster monster5 = new Monster(new StatisticsWarhammer(), new Experience(0), new StruggleStatisticsWarhammer());
+        monster5.setName("Zdzis");
         
         EntityManager.getInstance().addCreature(monster1);
         EntityManager.getInstance().addCreature(monster2);
         EntityManager.getInstance().addCreature(monster3);
+        EntityManager.getInstance().addCreature(monster4);
+        EntityManager.getInstance().addCreature(monster5);
     }
 }
--- src/game/utils/seeders/PlayerSeeder.java
+++ src/game/utils/seeders/PlayerSeeder.java
@@ -12,7 +12,19 @@ public class PlayerSeeder implements Seeder {
     public void seed() {
         PlayerCharacter playerCharacter1 = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(0), new StruggleStatisticsWarhammer());
         playerCharacter1.setName("Michalik");
+        PlayerCharacter playerCharacter2 = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(0), new StruggleStatisticsWarhammer());
+        playerCharacter2.setName("Adamow");
+        PlayerCharacter playerCharacter3 = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(0), new StruggleStatisticsWarhammer());
+        playerCharacter3.setName("Iwaszkiewicz");
+        PlayerCharacter playerCharacter4 = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(0), new StruggleStatisticsWarhammer());
+        playerCharacter4.setName("Hawking");
+        PlayerCharacter playerCharacter5 = new PlayerCharacter(new StatisticsWarhammer(), new Inventory(), new Experience(0), new StruggleStatisticsWarhammer());
+        playerCharacter5.setName("Lipton");
         
         EntityManager.getInstance().addCreature(playerCharacter1);
+        EntityManager.getInstance().addCreature(playerCharacter2);
+        EntityManager.getInstance().addCreature(playerCharacter3);
+        EntityManager.getInstance().addCreature(playerCharacter4);
+        EntityManager.getInstance().addCreature(playerCharacter5);
     }
 }
