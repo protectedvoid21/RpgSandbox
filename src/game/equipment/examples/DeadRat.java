0ad924a67f85a0a0ddeb2f8d15f5c93c323481ed
protectedvoid21 <nieznanyzero@interia.pl>
2023-01-24 22:43:43 +0100

    Fix DeadRat constructor

--- src/game/equipment/examples/DeadRat.java
+++ src/game/equipment/examples/DeadRat.java
@@ -3,7 +3,7 @@ package game.equipment.examples;
 import game.equipment.DisposableItem;
 
 public class DeadRat extends DisposableItem {
-    public DeadRat(int usageCount) {
-        super(usageCount);
+    public DeadRat(String name, int usageCount) {
+        super(name, usageCount);
     }
 }
